9b18446866c5d1329cb6228f977c3c9bc09e429c
Jon Zeolla <zeolla@gmail.com>
2017-03-06 14:38:12 -0500

    Try 1 for centos7

--- setup/setup.sh
+++ setup/setup.sh
@@ -77,7 +77,7 @@ versions[supported]="0.3.0","0.3.1"
 versions[workaround]="0.3.0","0.3.1"
 
 ## Populate additional, more dynamic variables
-if command -v python > /dev/null 2>&1 && [[ "Python ${component[python]}" == "$(python --version 2>&1)" ]]; then prereqs[python]="Expected"; else prereqs[python]="Unknown"; fi
+if command -v /usr/local/bin/python > /dev/null 2>&1 && [[ "Python ${component[python]}" == "$(/usr/local/bin/python --version 2>&1)" ]]; then prereqs[python]="Expected"; else prereqs[python]="Unknown"; fi
 if command -v easy_install-${component[python]:0:3} > /dev/null 2>&1 ; then prereqs[ez_setup]="Expected"; else prereqs[ez_setup]="Unknown"; fi
 if command -v ansible > /dev/null 2>&1 && [[ "ansible ${component[ansible]}" == "$(ansible --version | head -1)" ]]; then prereqs[ansible]="Expected"; else prereqs[ansible]="Unknown"; fi
 if command -v mvn > /dev/null 2>&1 && [[ "Apache Maven ${component[maven]}" == "$(mvn --version | head -1 | awk '{print $1,$2,$3}')" ]]; then prereqs[maven]="Expected"; else prereqs[maven]="Unknown"; fi
@@ -196,6 +196,8 @@ function _managePackages() {
     case "${1}" in
         install)
             action="install" ;;
+        groupinstall)
+            action="groupinstall" ;;
         update)
             action="update" ;;
         *)
@@ -427,7 +429,11 @@ case "${OSTYPE}" in
         if [[ -r /etc/centos-release ]]; then
             OS[distro]="$(awk -F\  '{print $1}' /etc/centos-release)"
             OS[version]="$(awk -F\  '{print $(NF-1)}' /etc/centos-release)"
-            if [[ "${OS[distro]}" == "CentOS" && ("${OS[version]}" == "6.8" || "${OS[version]}" =~ "7.2") ]]; then
+            # If a new version of CentOS 6 comes out and you want to support it, revisit the check below in the "Setup Metron" block
+            if [[ "${OS[distro]}" == "CentOS" && ("${OS[version]}" == "6.8" || "${OS[version]}" =~ "7.3") ]]; then
+                if [[ "${component[metron]}" == "master" && "${OS[version]}" == "6.8" ]]; then
+                    _feedback VERBOSE "As of METRON-671 CentOS 6 is no longer supported due to its dependency on docker.  We can't be sure that code will be in use here until later in the script, so I will continue..."
+                fi
                 OS[supported]="true"
             else
                 OS[supported]="false"
@@ -542,7 +548,7 @@ fi
 if [[ "${usetheforce}" != "1" ]]; then
     _feedback VERBOSE "Asking the user for confirmation"
     while [ -z "${prompt}" ]; do
-        read -p "This script is intended to be run on a fresh CentOS 6.8 or 7.2 installation and may have unintended side effects otherwise.  Do you want to continue (y/N)? " prompt
+        read -p "This script is intended to be run on a fresh CentOS 6.8 or 7.3 installation and may have unintended side effects otherwise.  Do you want to continue (y/N)? " prompt
         case "${prompt}" in
             [yY]|[yY][eE][sS])
                 _feedback INFO "Please note that this script may take a long time (60+ minutes) to complete"
@@ -606,6 +612,7 @@ else
     ./configure --prefix=/usr/local --enable-unicode=ucs4 --enable-shared LDFLAGS="-Wl,-rpath /usr/local/lib" || _feedback ERROR "Unable to configure python"
     make && sudo make altinstall || _feedback ERROR "Unable to \`sudo make altinstall\` python"
     sudo ln -fs "/usr/local/bin/python${component[python]:0:3}" /usr/local/bin/python || _feedback ERROR "Unable to link python${component[python]:0:3} to /usr/local/bin/python"
+    sudo yum -y install python-pip
 fi
 
 # Setup ez_setup
@@ -617,16 +624,15 @@ else
     _downloadit "https://bootstrap.pypa.io/ez_setup.py"
     sudo /usr/local/bin/python ez_setup.py || _feedback ERROR "Unable to setup ez_python.py"
     sudo "/usr/local/bin/easy_install-${component[python]:0:3}" pip || _feedback ERROR "Unable to setup pip"
-    sudo /usr/local/bin/pip -q install virtualenv paramiko PyYAML Jinja2 httplib2 six setuptools || _feedback ERROR "Unable to install tools with pip"
+    sudo pip -q install virtualenv paramiko PyYAML Jinja2 httplib2 six setuptools || _feedback ERROR "Unable to install tools with pip"
 fi
-
 
 # Setup ansible
 if [[ "${prereqs[ansible]}" == "Expected" ]]; then
     _feedback INFO "Ansible ${component[ansible]} already appears to be active, skipping..."
 else
     _feedback VERBOSE "Installing ansible using pip"
-    sudo /usr/local/bin/pip -q install "ansible==${component[ansible]}" || _feedback ERROR "Unable to install ansible"
+    sudo pip -q install "ansible==${component[ansible]}" || _feedback ERROR "Unable to install ansible"
 fi
 
 # Setup maven
@@ -670,6 +676,15 @@ else
     vagrant plugin install vagrant-hostmanager || _feedback ERROR "Unable to install the vagrant-hostmanager vagrant plugin"
 fi 
 
+# Setup docker
+if [[ "${OS[distro]}" == "CentOS" && "${OS[version]}" == "7.3" ]]; then
+    # Currently there isn't a specific version of docker that is required, so installing the latest ce
+    sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    sudo yum makecache fast
+    sudo yum install docker-ce
+    sudo systemctl start docker
+fi
+
 # Setup Metron
 _feedback VERBOSE "Installing metron into $(_getDir "metron")"
 # TODO: Allow a way to pull down and setup a specific, older version by checking out the ref
@@ -691,8 +706,12 @@ elif [[ "${mergepr}" == "1" ]]; then
         git fetch origin "pull/${prSpecified}/head:pr-${prSpecified}" || _feedback ERROR "Issue fetching the ${prSpecified} PR"
         git merge "pr-${prSpecified}" || _feedback ERROR "Issue merging the ${prSpecified} PR"
     else
-        _feedback "ABORT" "Something went wrong when trying to merge pr ${prSpecified} into $(_getDir "metron")"
+        _feedback ABORT "Something went wrong when trying to merge pr ${prSpecified} into $(_getDir "metron")"
     fi
+fi
+# Check for CentOS 6.8 and METRON-671
+if [[ $(git log --oneline | grep 'METRON-671' > /dev/null) && "${OS[distro]}" == "CentOS" && "${OS[version]}" == "6.8" ]]; then
+    _feedback ABORT "CentOS 6.8 is no longer compatible as of METRON-671"
 fi
 if [[ "${buildthedocs}" == "1" && $(grep "^metron_version: " "$(_getDir "metron")/metron-deployment/inventory/${deployChoice}/group_vars/all" | awk '{print $NF}') != "0.3.0" ]]; then
     _feedback VERBOSE "Building the related Metron docs"
